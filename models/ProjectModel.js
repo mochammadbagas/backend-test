bd78746a7a945cf7cbf667b89fba3c6ebfb61e1e
Mochammad Bagas <contact.mochammadbagas@gmail.com>
2023-02-08 14:02:43 +0700

    Add Project Model

---
+++ models/ProjectModel.js
@@ -0,0 +1,64 @@
+import { Sequelize } from 'sequelize';
+import db from '../config/Database';
+import Users from './UserModel';
+
+const { DataTypes } = Sequelize;
+
+const Projects = db.define(
+    'projects',
+    {
+        uuid: {
+            type: DataTypes.STRING,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            },
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            },
+        },
+        image: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            },
+        },
+        src: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            },
+        },
+        url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            },
+        },
+
+        //Relasi ke tabel User
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            },
+        },
+    },
+    {
+        freezeTableName: true,
+    }
+);
+
+Users.hasMany(Projects);
+Projects.belongsTo(Users, { foreignKey: 'userId' });
+
+export default Projects;
