3f797bba280c2fb6c3d3a311ff90c615e77fc202
Mochammad Bagas <contact.mochammadbagas@gmail.com>
2023-02-08 15:38:12 +0700

    Database Sync

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ import express from 'express';
 import cors from 'cors';
 import session from 'express-session';
 import dotenv from 'dotenv';
+import db from './config/Database.js';
+import UserRoute from './routes/UserRoute.js';
+import ProjectRoute from './routes/ProjectRoute.js';
 
 dotenv.config();
 
 const app = express();
+
+(async () => {
+    await db.sync();
+})();
 
 app.use(
     session({
@@ -26,6 +33,10 @@ app.use(
 );
 
 app.use(express.json());
+
+app.use(UserRoute);
+
+app.use(ProjectRoute);
 
 app.listen(process.env.APP_PORT, () => {
     console.log(
