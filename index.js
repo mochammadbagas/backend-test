21f21a16452b8b63d8e04a297094df3fda27e43e
Mochammad Bagas <contact.mochammadbagas@gmail.com>
2023-02-15 20:28:27 +0700

    Add Connect Session Sequelize

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import session from 'express-session';
 import dotenv from 'dotenv';
-// import db from './config/Database.js';
+import db from './config/Database.js';
+import SequelizeStore from 'connect-session-sequelize';
 import UserRoute from './routes/UserRoute.js';
 import ProjectRoute from './routes/ProjectRoute.js';
 import AuthRoute from './routes/AuthRoute.js';
@@ -10,6 +11,12 @@ import AuthRoute from './routes/AuthRoute.js';
 dotenv.config();
 
 const app = express();
+
+const sessionStore = SequelizeStore(session.Store);
+
+const store = new sessionStore({
+    db: db,
+});
 
 // (async () => {
 //     await db.sync();
@@ -20,6 +27,7 @@ app.use(
         secret: process.env.SESS_SECRET,
         resave: false,
         saveUninitialized: true,
+        store: store,
         cookie: {
             secure: 'auto',
         },
@@ -40,6 +48,8 @@ app.use(UserRoute);
 app.use(ProjectRoute);
 
 app.use(AuthRoute);
+
+// store.sync();
 
 app.listen(process.env.APP_PORT, () => {
     console.log(
