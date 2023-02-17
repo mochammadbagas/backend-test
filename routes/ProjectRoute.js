901399e022b81040fe19168415a37cd2e65a56a6
Mochammad Bagas <contact.mochammadbagas@gmail.com>
2023-02-17 20:36:53 +0700

    Add Middleware to Project Route

--- routes/ProjectRoute.js
+++ routes/ProjectRoute.js
@@ -6,13 +6,14 @@ import {
     updateProject,
     deleteProject,
 } from '../controllers/Projects.js';
+import { verifyUser } from '../middleware/AuthUser.js';
 
 const router = express.Router();
 
 router.get('/projects', getProjects);
 router.get('/projects/:id', getProjectById);
-router.post('/projects', createProject);
-router.patch('/projects/:id', updateProject);
-router.delete('/projects/:id', deleteProject);
+router.post('/projects', verifyUser, createProject);
+router.patch('/projects/:id', verifyUser, updateProject);
+router.delete('/projects/:id', verifyUser, deleteProject);
 
 export default router;
